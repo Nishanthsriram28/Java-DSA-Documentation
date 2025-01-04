a6c0a63a073e398f4fbb1e9de944d24444cfbde9
Nishanth Sriram <156437664+Nishanthsriram28@users.noreply.github.com>
2025-01-04 23:39:48 +0530

    Update KKfunctions.java

--- 02-Functions/KKfunctions.java
+++ 02-Functions/KKfunctions.java
@@ -68,7 +68,30 @@ public class KKfunctions {
         return  2*2.14*b;
     }
 
-    // 
+     // Prime or Not
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        primen(n);
+        if (primen(n)) {
+            System.out.println("Prime Number");
+        } else {
+            System.out.println("Not a Prime Nubmer");
+        }
+    }
+    public static boolean primen(int n) {
+        boolean prime = true;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+
+    
 
 }
 
