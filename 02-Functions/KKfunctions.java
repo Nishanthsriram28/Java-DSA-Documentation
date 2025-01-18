357e9332c18d6d57bb31fe1838898c996b351a78
Nishanth Sriram <156437664+Nishanthsriram28@users.noreply.github.com>
2025-01-18 13:58:55 +0530

    Update KKfunctions.java

--- 02-Functions/KKfunctions.java
+++ 02-Functions/KKfunctions.java
@@ -215,6 +215,24 @@ public class KKfunctions {
                 }
                 return cubed==n;
         }
+
+        // 8.Triangle
+        public static void main(String[] args) {
+                triangle(5);
+        }
+        public static void triangle(int n) {
+                for (int i = 1; i <= n; i++) {
+                        for (int j = 1; j <= n - i; j++) {
+                                System.out.print(" ");
+                        }
+                        for (int k = 1; k <= 2 * i - 1; k++) {
+                                System.out.print("*");
+                        }
+                        System.out.println();
+                }
+        }
+
+    
   
 
 }
