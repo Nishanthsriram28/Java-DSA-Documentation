0af39c35d51452eff46dbdf000fb55a57f5d34f1
Nishanth Sriram <156437664+Nishanthsriram28@users.noreply.github.com>
2025-01-22 11:44:46 +0530

    Update KKfunctions.java

--- 02-Functions/KKfunctions.java
+++ 02-Functions/KKfunctions.java
@@ -247,6 +247,22 @@ public class KKfunctions {
         }
     }
 
+
+      // Spaced Inverted Triangle
+    public static void main(String[] args) {
+        int n=8;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n+1-i;j++){
+                System.out.print("*");
+                System.out.print(" ");
+            }
+            System.out.println();
+            for(int k=0;k<=i-1;k++){
+                System.out.print(" ");
+            }
+        }
+    }
+
     
   
 
