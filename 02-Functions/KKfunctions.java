0c07a5775e47b60cf0cbf5673f9c522a95113902
Nishanth Sriram <156437664+Nishanthsriram28@users.noreply.github.com>
2025-01-23 22:51:22 +0530

    Update KKfunctions.java

--- 02-Functions/KKfunctions.java
+++ 02-Functions/KKfunctions.java
@@ -263,6 +263,32 @@ public class KKfunctions {
         }
     }
 
+    //Hour glass pattern , but not setting correctly
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int k=0;k<=i-1;k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n+1-i;j++){
+                System.out.print("* ");
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        int N=n-1;
+        for(int i=1;i<=N;i++){
+            for(int k=1;k<=N-i;k++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
     
   
 
