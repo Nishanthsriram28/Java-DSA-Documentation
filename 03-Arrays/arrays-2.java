ff1097755438824322f73823aef67a9512d514df
Nishanth Sriram <156437664+Nishanthsriram28@users.noreply.github.com>
2025-03-08 14:42:53 +0530

    Update arrays-2.java

--- 03-Arrays/arrays-2.java
+++ 03-Arrays/arrays-2.java
@@ -32,5 +32,20 @@ public static int TotalTrappedWater(int arr[]){
     }
 
 
-
+// Buy & Sell Stock
+    public static int buysell(int arr[]){
+        int maxprofit=0, bestbuy = arr[0];
+        int n = arr.length;
+        for(int i=1;i<n;i++){
+            if(arr[i]>bestbuy){
+                maxprofit = Math.max(maxprofit,arr[i]-bestbuy);
+            }
+            bestbuy = Math.min(bestbuy,arr[i]);
+        }
+        return maxprofit;
+    }
+    public static void main(String[] args){
+        int arr[] = {7,1,5,3,6,4};
+        System.out.println(buysell(arr));
+    }
 
